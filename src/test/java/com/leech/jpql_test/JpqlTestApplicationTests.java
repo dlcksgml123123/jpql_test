70ca63511c23afb51e89af03338923cd1ae87901
dlcksgml123123 <qwedcxza0501@naver.com>
2022-12-16 09:59:50 +0900

    시작

---
+++ src/test/java/com/leech/jpql_test/JpqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leech.jpql_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
